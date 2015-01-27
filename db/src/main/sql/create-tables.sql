b29a0f5dab51cf95208ee34e85d06d689a21f1d0
kcoolsae <kris.coolsaet@ugent.be>
2015-01-27 16:34:35 +0100

    Fixed some small errors

--- db/src/main/sql/create-tables.sql
+++ db/src/main/sql/create-tables.sql
@@ -92,7 +92,7 @@ CREATE TABLE `users` (
 	`user_driver_license_date` DATE,
 	`user_identity_card_id` VARCHAR(32), -- Identiteitskaartnr
 	`user_identity_card_registration_nr` VARCHAR(32), -- Rijksregisternummer
-	`user_status` ENUM('EMAIL_VALIDATING','REGISTERED','FULL_VALIDATING','FULL','BLOCKED','DROPPED','INACTIVE') NOT NULL DEFAULT 'EMAIL_VALIDATING', -- Stadia die de gebruiker moet doorlopen
+	`user_status` ENUM('REGISTERED','FULL_VALIDATING','FULL','BLOCKED','DROPPED','INACTIVE') NOT NULL DEFAULT 'REGISTERED', -- Stadia die de gebruiker moet doorlopen
 	`user_damage_history` TEXT,
 	`user_agree_terms` BIT(1),
 	`user_image_id` INT,
